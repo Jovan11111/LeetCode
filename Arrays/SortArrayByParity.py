6b93b88dbc18328b3703ac546fca0b73e961bc1a
Jovan Janković <hongjongjo@gmail.com>
2025-02-02 18:24:21 +0100

    Update SortArrayByParity.py

--- Arrays/SortArrayByParity.py
+++ Arrays/SortArrayByParity.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/sort-array-by-parity/
+
+# Moje resenje
+# O(n) O(1) - najbolje
 class Solution(object):
     def sortArrayByParity(self, nums):
         start = 0
@@ -5,10 +9,9 @@ class Solution(object):
         while(start <= end):
             if nums[start] % 2 == 1 and nums[end] % 2 == 0:
                 nums[start], nums[end] = nums[end], nums[start]
-            else:
-                if nums[start] % 2 == 0:
-                    start += 1
-                if nums[end] % 2 == 1:
-                    end -= 1
+            if nums[start] % 2 == 0:
+                start += 1
+            if nums[end] % 2 == 1:
+                end -= 1
         return nums
         
