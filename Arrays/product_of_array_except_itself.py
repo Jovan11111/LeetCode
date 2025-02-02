4f03789782489532595c00dca25e5dbad54f2710
Jovan Janković <hongjongjo@gmail.com>
2025-02-02 18:31:59 +0100

    Update product_of_array_except_itself.py

--- Arrays/product_of_array_except_itself.py
+++ Arrays/product_of_array_except_itself.py
@@ -1,6 +1,6 @@
-"""
-My solution
-"""
+# https://leetcode.com/problems/product-of-array-except-self/
+# Moje resenje
+# Optimalno al koristi deljenje, nije poenta da koristi deljenje
 class Solution(object):
     def productExceptSelf(self, nums):
         full_product = 1
@@ -22,8 +22,25 @@ class Solution(object):
                     nums[i] = 0
                 else:
                     nums[i] = full_product / nums[i]
-        return nums
-    
-"""
-Comments
-"""
+        return 
+
+# Najbolje resenje
+# Jako pametno, u prvom prolazu racuna proizvod levo od nekog elementa, i onda u drugom prolazu se racuna proizvod desno od tog elementa, i onda se ta dva pomnoze
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        result = [1] * n  # Početna vrednost
+
+        # Proizvod elemenata levo od svakog elementa
+        left_product = 1
+        for i in range(n):
+            result[i] = left_product
+            left_product *= nums[i]
+
+        # Proizvod elemenata desno od svakog elementa
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+
+        return result
