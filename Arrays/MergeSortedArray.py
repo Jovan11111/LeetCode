189f9617787bb423add8745f46e40526bd25a703
Jovan Janković <hongjongjo@gmail.com>
2025-02-02 18:10:04 +0100

    Update MergeSortedArray.py

--- Arrays/MergeSortedArray.py
+++ Arrays/MergeSortedArray.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/merge-sorted-array/
+# Moje resenje
+# O(m + n) O(m + n). Nije lose resenje, ali moze bolje. Zauzima se dodatan prostor iako ne bi trebalo
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         p1 = 0
@@ -18,3 +21,27 @@ class Solution(object):
                 helper.append(nums1[i])
         for i in range(len(helper)):
             nums1[i] = helper[i]
+
+# Najbolje resenje
+# Slicno kao i dupliranje nula, isto se ide unazad i procenjuje se dokle ce ici taj niz
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        p1 = m - 1          # Pokazivač na poslednji validni element u nums1
+        p2 = n - 1          # Pokazivač na poslednji element u nums2
+        p = m + n - 1       # Pokazivač na poslednju poziciju u nums1 (on ima one dodatne nule)
+
+        # Ide se unazad dok su oni veci od nule, i uzima se veci i stavlja se na kraj. Onda se dodaje i dodaje i eto
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        # Dodaju se elementi koji su preostali. Ne mora da se radi za nums1, jer su oni vec na mestu, u njih se ubacuju ovi iz nums2
+        # Ako su preostali elementi u nums2 (nums1 su već na mestu)
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
