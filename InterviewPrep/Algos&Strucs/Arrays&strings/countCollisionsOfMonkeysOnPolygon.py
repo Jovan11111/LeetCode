95e4351658b1c4b4fcc5b53708f03df7b610dbca
Jovan Janković <hongjongjo@gmail.com>
2025-05-13 23:10:40 +0200

    Create countCollisionsOfMonkeysOnPolygon.py

---
+++ InterviewPrep/Algos&Strucs/Arrays&strings/countCollisionsOfMonkeysOnPolygon.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/count-collisions-of-monkeys-on-a-polygon/
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def binary_rep(num):
+            ret = []
+            while num != 0:
+                ret.append(num % 2)
+                num = num // 2
+            return ret
+        ret = []
+        for i in range(2**len(nums)):
+            mask = binary_rep(i)
+            ret.append([nums[j] for j in range(len(mask)) if mask[j] == 1])
+        return ret
